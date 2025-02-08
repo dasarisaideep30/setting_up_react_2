2ab49160195bd233e8a22c5736ff23252271934e
Sai <saideepdasari30@gmail.com>
2025-02-08 09:17:27 +0530

    test commit

---
+++ my-app/src/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Saideep</h1>
+      <p>Contact: saideep@example.com</p>
+    </div>
+  );
+};
+
+export default Home;
